dc5fd090511e45cbce7cee8264959c909a7455f0
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-03-23 20:17:00 +0530

    removed all svgs icon and used fontawesome cdn

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ let close_modal = document.getElementById('btn-close-modal')
 let modalBody = document.querySelector('.modal__body')
 btn.addEventListener('click',() => {
     modal.style.display = 'block';
-    document.body.style.backgroundColor =' rgba(0,0,0,0.5)'
+    
 })
 modal_close.addEventListener('click',() =>{
     modal.style.display = 'none';
-    document.body.style.backgroundColor ='transparent'
+    
 })
 close_modal.addEventListener('click',() =>{
     modal.style.display = 'none';
-    document.body.style.backgroundColor ='transparent'
 })
