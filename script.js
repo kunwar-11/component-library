c8ca5edb6aebd956d0c514cdbf107a8f92bcdbfa
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2021-03-24 11:38:55 +0530

    added comments ,  functionality for snackbar , refactor styles

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let modal = document.querySelector('.modal')
 let modal_close = document.querySelector('.modal__close')
 let close_modal = document.getElementById('btn-close-modal')
 let modalBody = document.querySelector('.modal__body')
+let openSnackbarSuccess = document.getElementById('open-snackbar-success')
+let openSnackbarSecondary = document.getElementById('open-snackbar-secondary')
+let snackbarPrimary = document.querySelector('.snackbar__primary')
+let snackbarSecondary = document.querySelector('.snackbar__secondary')
+let closeSnackbarPrimary = document.querySelector('.close__snackbar__primary');
+let closeSnackbarSecondary = document.querySelector('.close__snackbar__secondary');
+
+
 btn.addEventListener('click',() => {
     modal.style.display = 'block';
     
@@ -13,4 +21,18 @@ modal_close.addEventListener('click',() =>{
 })
 close_modal.addEventListener('click',() =>{
     modal.style.display = 'none';
+})
+openSnackbarSuccess.addEventListener('click' , () => {
+    snackbarPrimary.style.display = 'flex'
+    setTimeout(() => snackbarPrimary.style.display = 'none' , 2500)
+})
+openSnackbarSecondary.addEventListener('click' , () => {
+    snackbarSecondary.style.display = 'flex'
+    setTimeout(() => snackbarSecondary.style.display = 'none' , 2500)
+})
+closeSnackbarPrimary.addEventListener('click' , () => {
+    snackbarPrimary.style.display = 'none'
+})
+closeSnackbarSecondary.addEventListener('click' , () => {
+    snackbarSecondary.style.display = 'none'
 })
